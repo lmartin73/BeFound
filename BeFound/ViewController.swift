94d84e96b78fa6ab989322379fadfb642579045d
dev <luther.d.martin@gmail.com>
2017-04-20 10:34:20 -0400

    initial commit with mapkit

--- BeFound/ViewController.swift
+++ BeFound/ViewController.swift
@@ -7,18 +7,45 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        requestLocationAccess(); 
         // Do any additional setup after loading the view, typically from a nib.
+//        var point = MKPointAnnotation();
+//        point.coordinate = CLLocationCoordinate2D(latitude: 32.462244, longitude: -90.186963)
+//        point.title = "My Home";
+//        
+//        mapView.addAnnotation(point)
+//        let regionRadius: CLLocationDistance = 1000
+//        let coordinateRegion = MKCoordinateRegionMakeWithDistance(point.coordinate,
+//                                                                  regionRadius * 2.0, regionRadius * 2.0)
+//        mapView.setRegion(coordinateRegion, animated: true)
+//        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func requestLocationAccess() {
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return
+            
+        case .denied, .restricted:
+            print("location access denied")
+            
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
     }
+    
 
 
 }
